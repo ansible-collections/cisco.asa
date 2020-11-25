72870041f37cf54461b91f91e67ee11a2efc0e65
Sumit Jaiswal <sjaiswal@redhat.com>
2020-11-25 20:29:08 +0530

    To fix ASA OGs module where delete by name was not resulting to an expected behaviour  (#77)
    To fix ASA OGs module where delete by name was not resulting to an expected behaviour
    
    Reviewed-by: https://github.com/apps/ansible-zuul

--- tests/unit/modules/network/asa/test_asa_ogs.py
+++ tests/unit/modules/network/asa/test_asa_ogs.py
@@ -283,6 +283,22 @@ class TestAsaOGsModule(TestAsaModule):
         )
         self.execute_module(changed=False, commands=[], sort=True)
 
+    def test_asa_ogs_delete_by_name(self):
+        set_module_args(
+            dict(
+                config=[
+                    dict(
+                        object_groups=[dict(name="test_og_network")],
+                        object_type="network",
+                    )
+                ],
+                state="deleted",
+            )
+        )
+        result = self.execute_module(changed=True)
+        commands = ["no object-group network test_og_network"]
+        self.assertEqual(result["commands"], commands)
+
     def test_asa_ogs_deleted_all(self):
         set_module_args(dict(state="deleted"))
         result = self.execute_module(changed=True)
