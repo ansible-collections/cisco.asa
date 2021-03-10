2b926f08df080debc7e8711a596a09db07480b5b
doge-tech <65607722+doge-tech@users.noreply.github.com>
2021-03-10 13:24:12 +0000

    asa_acl.py: Remove spurious whitespace

--- plugins/modules/asa_acl.py
+++ plugins/modules/asa_acl.py
@@ -211,7 +211,7 @@ def main():
             commands = candidate.difference(config)
             commands = dumps(commands, "commands").split("\n")
             commands = [str(c) for c in commands if c]
-            
+
             if commands and module.params["replace"] == "block":
                 commands = str(candidate).split("\n")
         else:
