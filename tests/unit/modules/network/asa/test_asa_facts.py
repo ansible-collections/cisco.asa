0a95f01a505c5fde50093cde220fa12cfb9a606c
Tim Way <1091435+timway@users.noreply.github.com>
2021-08-25 23:42:56 -0500

    Fix Remaining Sanity Test Failures
    - This addresses the following 2 errors in the 'pylint' test:
    
    ```
    ERROR: tests/unit/mock/loader.py:49:4: arguments-renamed: Parameter 'file_name' has been renamed to 'path' in overridden 'DictDataLoader._get_file_contents' method
    ERROR: tests/unit/modules/network/asa/test_asa_facts.py:72:27: use-maxsplit-arg: Use str(command).split(' | ', maxsplit=1)[0] instead 2021-08-25 06:08:57.518030 | centos-8-stream
    ```

--- tests/unit/modules/network/asa/test_asa_facts.py
+++ tests/unit/modules/network/asa/test_asa_facts.py
@@ -69,7 +69,9 @@ class TestAsaFactsModule(TestAsaModule):
             output = list()
 
             for command in commands:
-                filename = str(command).split(" | ")[0].replace(" ", "_")
+                filename = (
+                    str(command).split(" | ", maxsplit=1)[0].replace(" ", "_")
+                )
                 output.append(load_fixture("asa_facts_%s" % filename))
             return output
 
