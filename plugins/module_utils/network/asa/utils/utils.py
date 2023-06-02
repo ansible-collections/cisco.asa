aa65bf1aefed884264458035b04f2263cc02f577
nleiva <nicolasleiva@gmail.com>
2023-06-02 13:04:03 -0400

    Change utils 2 ansible.module_utils.common.network

--- plugins/module_utils/network/asa/utils/utils.py
+++ plugins/module_utils/network/asa/utils/utils.py
@@ -12,7 +12,7 @@ __metaclass__ = type
 
 import socket
 from ansible.module_utils.six import iteritems
-from ansible_collections.ansible.netcommon.plugins.module_utils.network.common.utils import (
+from ansible.module_utils.common.network import (
     is_masklen,
     to_netmask,
 )
